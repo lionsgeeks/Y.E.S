6ff172b50abc59300ea648a288c279fd2fc222f0
yahya mws <yahyamoussair05@gmail.com>
2025-10-10 12:28:50 +0100

    fix : try fix the ci test

--- resources/js/pages/about/partials/SponsorsSection.jsx
+++ resources/js/pages/about/partials/SponsorsSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, FreeMode } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
